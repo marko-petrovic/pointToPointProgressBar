fdbc2bd0daab12e3d202713f9719a5709661eb5a
marko-petrovic <marko.petrovic.najnormalnije@gmail.com>
2017-02-08 21:59:29 +0100

    Initial commit

---
+++ app/src/main/java/com/dualquo/te/pointtopointprogressbar/PointToPointProgressBarSampleActivity.java
@@ -0,0 +1,13 @@
+package com.dualquo.te.pointtopointprogressbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointToPointProgressBarSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_to_point_progress_bar_sample);
+    }
+}
